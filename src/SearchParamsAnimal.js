545a33d35412f6c7b7f6ef1531fd76ebdffaf4c5
manan_rana@hotmail.com <mananrana@Manans-MacBook-Pro.local>
2021-07-25 19:57:41 +1000

    added use effect and fetch api

--- src/SearchParamsAnimal.js
+++ src/SearchParamsAnimal.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -6,8 +7,23 @@ const SearchParamsAnimal = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [animal, updateAnimal] = useState("");
   const [breed, updateBreed] = useState("");
+  const [pets, setPets] = useState([]);
 
   const breeds = [];
+
+  useEffect(() => {
+    requestPets();
+  }, []);             //using the square brackets you effectively use call it once
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+    console.log(json);
+
+    setPets(json.pets);
+  }
 
   // if location is Seattle then set it to true for isRaining,
   // such conditions can be set in the fucntion as well
@@ -38,8 +54,10 @@ const SearchParamsAnimal = () => {
             onChange={(e) => updateAnimal(e.target.value)}
             onBlur={(e) => updateAnimal(e.target.value)}
           >
-            <option />                    // the empty option at the start
-            {ANIMALS.map((animal) => (    //map loops throught the array
+            <option /> // the empty option at the start
+            {ANIMALS.map((
+              animal //map loops throught the array
+            ) => (
               <option key={animal} value={animal}>
                 {animal}
               </option>
@@ -54,8 +72,10 @@ const SearchParamsAnimal = () => {
             onChange={(e) => updateBreed(e.target.value)}
             onBlur={(e) => updateBreed(e.target.value)}
           >
-            <option />                    // the empty option at the start
-            {breeds.map((breed) => (    //map loops throught the array
+            <option /> // the empty option at the start
+            {breeds.map((
+              breed //map loops throught the array
+            ) => (
               <option key={breed} value={breed}>
                 {breed}
               </option>
@@ -64,6 +84,11 @@ const SearchParamsAnimal = () => {
         </label>
         <button>Submit</button>
       </form>
+      {
+        pets.map((pet) => (
+          <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+        ))
+      }
     </div>
   );
 };
